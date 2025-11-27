fdbda4cf71763cf19c194409717a2ae130d325e1
Jelly <ezway123@gmail.com>
2025-11-27 23:52:16 +0800

    階段工作完成。

--- basic/css_demo/expand/s3.js
+++ basic/css_demo/expand/s3.js
@@ -1,11 +1,11 @@
 function enter() {
-  document.querySelector(".div1").classList.add("div1-move")
-  document.querySelector(".div2").classList.add("div2-move")
-  document.querySelector(".div3").classList.add("div3-move")
+    document.querySelector(".div1").classList.add("div1-move")
+    document.querySelector(".div2").classList.add("div2-move")
+    document.querySelector(".div3").classList.add("div3-move")
 }
 
 function leave() {
-  document.querySelector(".div1").classList.remove("div1-move")
-  document.querySelector(".div2").classList.remove("div2-move")
-  document.querySelector(".div3").classList.remove("div3-move")
+    document.querySelector(".div1").classList.remove("div1-move")
+    document.querySelector(".div2").classList.remove("div2-move")
+    document.querySelector(".div3").classList.remove("div3-move")
 }
